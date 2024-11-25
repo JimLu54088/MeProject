700ccb695b00f92589f53c5de6b6dc13b2aebf61
JIMLU001234 <xm3ruxup6vupm4@gmail.com>
2024-11-25 18:53:32 +0900

    delete springboot-batch

--- springboot-batch/src/main/java/jp/co/jim/common/AppFactory.java
+++
@@ -1,8 +0,0 @@
-package jp.co.jim.common;
-
-import jp.co.jim.processor.BatchProcessorIf;
-
-public interface AppFactory {
-
-    public BatchProcessorIf createInstance(String className);
-}
--- springboot-batch/src/main/java/jp/co/jim/processor/BatchProcessorIf.java
+++
@@ -1,6 +0,0 @@
-package jp.co.jim.processor;
-
-public interface BatchProcessorIf {
-
-    public void process() throws Throwable;
-}
